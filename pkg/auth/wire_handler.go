7ffb81beb7a89c5ed2b986fa5ec65b834a5ae8be
YinYin Chiu <cychiuae@connect.ust.hk>
2024-09-12 18:21:22 +0800

    Scaffold list username identities page

--- pkg/auth/wire_handler.go
+++ pkg/auth/wire_handler.go
@@ -1249,3 +1249,10 @@ func newWebAppAuthflowV2SettingsIdentityListEmailHandler(p *deps.RequestProvider
 		wire.Bind(new(http.Handler), new(*handlerwebappauthflowv2.AuthflowV2SettingsIdentityListEmailHandler)),
 	))
 }
+
+func newWebAppAuthflowV2SettingsIdentityListUsernameHandler(p *deps.RequestProvider) http.Handler {
+	panic(wire.Build(
+		DependencySet,
+		wire.Bind(new(http.Handler), new(*handlerwebappauthflowv2.AuthflowV2SettingsIdentityListUsernameHandler)),
+	))
+}
